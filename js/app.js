0147a6d50b6fb6d43a682561eecad0f71394a184
Belkasmi Zakariae <48525328+zackthe@users.noreply.github.com>
2021-01-03 15:29:49 +0100

    flex modification

--- js/app.js
+++ js/app.js
@@ -11,3 +11,49 @@ navigation.classList.toggle('show');
 hamberger_menu.classList.toggle('open');    
     
     })
+
+
+/**** copy email to clipboard ****/
+
+let copybutton = document.querySelector('.footer-btn');
+let copytext = document.querySelector('.alert-msg');
+
+
+function copyToClipboard() {
+    var dummy = document.createElement("input");
+    document.body.appendChild(dummy);
+    dummy.setAttribute('value', 'belkasmizakariae@gmail.com');
+    dummy.select();
+    document.execCommand('copy');
+    document.body.removeChild(dummy);}
+
+
+
+copybutton.addEventListener('click',()=>{
+        copyToClipboard();
+        copytext.classList.add('alert-msg-show');
+     setTimeout(function(){
+        
+            copytext.classList.remove(`alert-msg-show`);
+
+    },2000);
+})
+
+
+
+function displayAlert(text,action){
+     Alert.childNodes[1].textContent= text;
+
+    Alert.classList.add(`alert-${action}`);
+    
+    setTimeout(function(){
+        
+             Alert.childNodes[1].textContent= '';
+
+            Alert.classList.remove(`alert-${action}`);
+
+    },1200);
+}
+
+
+ 
